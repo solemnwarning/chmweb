solemnwarning/chmweb

commit 6796fbccf384cd48d376deaaa6645334792280c4

Ignore weird .hhc/.hhk topics in chi files.

lib/App/ChmWeb/AKLinkTable.pm
@@ -474,6 +474,15 @@ sub _load_topics
 			# > ANSI/UTF-8 NT string that is the Local for this topic.
 			my ($urlstr_string) = $self->_file_read_utf8_string($urlstr_file, $urlstr_offset + 8);
 			
+			if(!defined($strings_string) && $urlstr_string =~ m/\.hh[kc]$/i)
+			{
+				# Some .chi files have topics which reference the .hhc inside the
+				# chm for some reason, this causes problems for us since it gets
+				# registered as a page, so just skip them.
+				
+				next;
+			}
+			
 			push(@topics, {
 				Name  => $strings_string,
 				Local => $path_prefix.$urlstr_string,

commit 0ac8e590047ad6530446af7fd5f4ae62c96b02c2

Fix off-by-one errors in AKLinkTable.
The change I did in 6796fbccf384cd48d376deaaa6645334792280c4 introduced
off-by-one errors in topic references.

lib/App/ChmWeb/AKLinkTable.pm
@@ -480,13 +480,20 @@ sub _load_topics
 				# chm for some reason, this causes problems for us since it gets
 				# registered as a page, so just skip them.
 				
-				next;
+				# next;
+				
+				# Nope, we don't skip them, because that would mess up the indices
+				# in the topics array, we insert an empty hash that hopefully won't
+				# break anything instead.
+				
+				push(@topics, {});
 			}
-			
-			push(@topics, {
-				Name  => $strings_string,
-				Local => $path_prefix.$urlstr_string,
-			});
+			else{
+				push(@topics, {
+					Name  => $strings_string,
+					Local => $path_prefix.$urlstr_string,
+				});
+			}
 		}
 		else{
 			# TODO: I don't think these are right...